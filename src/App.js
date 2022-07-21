de589f66690e8573914c4dc9cb54fb66339ce221
Dilek <dilekyal1@gmail.com>
2022-07-21 12:34:20 +0300

    added header and search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { Button } from '@mui/material';
+import { StyledEngineProvider } from '@mui/material';
+import Header from "./components/Header/Header"
+import Search from "./components/Search/Search"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Welcome!
-      <Button variant="contained">Hello World!</Button>
+      <StyledEngineProvider injectFirst>
+        <Header />
+        <main>
+          <Search />
+        </main>
+      </StyledEngineProvider>
     </div>
   );
 }
---
+++ src/components/Search/Search.js
@@ -0,0 +1,16 @@
+import Input from '@mui/material/Input';
+import TextField from '@mui/material/TextField';
+import "./Search.css"
+
+const Search = () => {
+
+    return (
+        <section className='search' id='search'>
+            <div className='search-items'>
+                <TextField type="text" placeholder='Ürün ara...' size='small' sx={{ width: "20rem"}}></TextField>
+            </div>    
+        </section>
+    );
+}
+
+export default Search;
