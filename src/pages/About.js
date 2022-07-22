3273611d55f6d4025f0e14e43aedf321b239ce66
cangnss <cgunes52@gmail.com>
2022-07-22 10:37:37 +0300

    added new features

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,52 @@
-const About = () => {
-    return (
-     <div>
-        About Page
-     </div>   
-    )
+import { Button, Paper } from "@mui/material";
+import Carousel from "react-material-ui-carousel/dist/components/Carousel";
+import React from "react";
+import "./About.css";
+
+function About(props) {
+  var items = [
+    {
+      name: "Biz Kimiz",
+      description: "ARKSOFT olarak, müşterilerimizin bilişim sektöründeki ihtiyaçlarına en uygun çözüm alternatiflerini geliştirerek, kendilerinin bir sonraki ve en iyi versiyonlarına geçmelerine yardımcı oluruz. Müşteri memnuniyeti en önemli odağımızdır. Yaptığımız işe ve müşterilerimiz üzerindeki etkimize derinden önem veriyoruz. İşlerimiz tamamen garantilidir ve uyguladığımız çözüm en güncel yöntemlerle ele alınır. Sorunları kısa sürede algılar, hızlı çözüm üretir ve aynı sorunun tekrarlanmamasını sağlarız.",
+    },
+    {
+      name: "Nasıl Çalışırız",
+      description: "Müşterilerimizin ihtiyaçlarını analiz edip, en uygun çözüm alternatiflerini oluşturup, karşılıklı olarak memnuniyeti sağladıktan sonra çalışmaya başlarız. Uygun olan çözüm, sisteme entegre edildikten sonra gerekli güncelleme ve bakım desteğiyle, çözümün sürekli gelişen bilişim teknolojilerine uyumunu gerçekleştiririz.",
+    },
+    {
+        name: "Vizyonumuz",
+        description: "Global iş ortaklarımız ve deneyimlerimizle, müşterilerin ihtiyacı olan bilişim sektöründe ortaya çıkan tüm ihtiyaçları, memnuniyeti en üst düzeyde tutarak, dürüst, samimi ve titiz yaklaşımımızla gidermek. Yurtdışı ve yurtiçi pazarında hizmetlerimizle yer almak ve dünyanın önde gelen bilişim kuruluşları ile stratejik iş ortaklıkları kurmak.",
+      },
+      {
+        name: "Misyonumuz",
+        description: "Uzman kadromuz ile yenilikçi, kaliteli, teknoloji gelişimine duyarlı, yüksek verimlilikte hazırlanmış çözüm ve hizmet planları sunarak, bilişim sektörüne nitelikli ve iş bilincine sahip personeller kazandırmak.",
+      },
+      {
+        name: "Değerlerimiz",
+        description: "Değerlerimiz, organizasyonumuzun kültürünü şekillendirir ve şirketimizin karakterini tanımlar."
+        
+      },
+  ];
+  return (
+    <div className="about">
+      <Carousel >
+        {items.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
+      </Carousel>
+    </div>
+  );
+}
+
+function Item(props) {
+  return (
+    <div className="items">
+        <Paper elevation={8} sx={{ height:"100%"}}>
+            <h2>{props.item.name}</h2>
+            <p>{props.item.description}</p>
+        </Paper>
+    </div>
+  );
 }
 
 export default About;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,106 @@
+import "./Contact.css";
+import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import EmailIcon from "@mui/icons-material/Email";
+import CallIcon from "@mui/icons-material/Call";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import { Button, TextareaAutosize } from "@mui/material";
 const Contact = () => {
-    return (
-     <div>
-        Contach Page
-     </div>   
-    )
-}
+  return (
+    <div className="contact">
+      <Grid
+        container
+        spacing={3}
+      >
+        <Grid item xs={12} md={6} lg={6} xl={5} sx={{ display:"flex", justifyContent:"center", alignItems:"center", flexDirection:"column" }}>
+          <h2 style={{ alignitems: "center", marginTop: "3.6rem" }}>
+            İLETİŞİM
+          </h2>
+          <Box
+            sx={{ mx:"auto" }}
+          >
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <CallIcon sx={{ color: "black" }} />
+                  </ListItemIcon>
+                  <ListItemText primary="+90 312 502 21 21" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <EmailIcon sx={{ color: "black" }} />
+                  </ListItemIcon>
+                  <ListItemText primary="info@arksoft.com.tr" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <LocationOnIcon sx={{ color: "black" }} />
+                  </ListItemIcon>
+                  <ListItemText sx={{ width: '20rem'}} primary="Üniversiteler Mahallesi, Hacettepe Teknokent Safir Blokları E Blok Kat: 9, Çankaya Cd. No:51, 06640" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6} lg={6} xl={7} sx={{ display:"flex", justifyContent:"center", alignItems:"center", marginTop:"2rem"}}>
+          <div id="contact-item2">
+            <div className="form-group">
+              <TextField
+                required
+                id="outlined-required"
+                label="Ad-Soyad"
+                placeholder="Ad-Soyad"
+                sx={{ width: "25rem" }}
+              />
+            </div>
+            <div className="form-group">
+              <TextField
+                required
+                id="outlined-required"
+                label="E-mail"
+                placeholder="E-mail"
+                sx={{ width: "25rem" }}
+              />
+            </div>
+            <div className="form-group">
+              <Box>
+                <TextField
+                  id="outlined-required"
+                  label="Konu"
+                  placeholder="Konu"
+                  sx={{ width: "25rem" }}
+                />
+              </Box>
+            </div>
+            <div className="form-group">
+              <Box>
+                <TextareaAutosize
+                  required
+                  aria-label="empty textarea"
+                  placeholder="Açıklama"
+                  style={{ width: "25rem", height: 50 }}
+                />
+              </Box>
+            </div>
+            <div style={{ marginTop: "2rem"}}>
+              <Button variant="contained">Gönder</Button>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default Contact;
